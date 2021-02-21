b5cdaf3db74153a43ab298dc4c75538d66c38700
Rahul Mishra <rahul@kali>
2021-02-21 12:47:14 +0530

    Update in name convention.
    Earlier the variable name that stores the input of the user was using camel case
    naming convention, now the naming convention the variable is changed to genreal
    naming convention of shell script.

--- script.sh
+++ script.sh
@@ -4,38 +4,38 @@
 
 # Asking for the domain name form the user
 echo -e "Please enter the DNS of the website: \c"
-read dnsName
+read dns_name
 
 # Displaying the  details of the IP address for the entered domain name
 echo ""
 echo "Displaying IP address details of the specified domain"
 echo ""
-host $dnsName
+host $dns_name
 
 # When the -C option is used, host will attempt to display the SOA records for zone name from all the listed authoritative name servers for that zone. The list of name servers is defined by the NS records that are found for the zone.
 echo ""
 echo "Displaying SOA records for the zone name form all the listed authorative name servers."
 echo ""
-host -C $dnsName
+host -C $dns_name
 
 
 # Verbous output is generated when we use -d or -v flag
 echo ""
 echo "Verbous output"
 echo ""
-host -d $dnsName
+host -d $dns_name
 
 # -t is used to specify the type of query and ns is used to get the details of the name server explicitly
 echo ""
 echo "Displaying the name servers for the entred domain name"
 echo ""
-host -t ns $dnsName
+host -t ns $dns_name
 
 # To print txt records 
 echo ""
 echo "Displaying the text record for the given domain name"
 echo ""
-host -t txt $dnsName
+host -t txt $dns_name
 
 
 
